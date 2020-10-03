59cdb69cba537b5598b6376451197653c360a197
RonaldKiprotich <protich12@gmail.com>
2020-10-03 18:23:19 +0300

    added functions that returns profile information and repos when clicked

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiKey: "62ccefed95b887b7408be1ab0011098282393364"
+  apiKey: "62ccefed95b887b7408be1ab0011098282393364",
+  apiUrl: 'https://api.github.com/users/',
 };
 
 /*
